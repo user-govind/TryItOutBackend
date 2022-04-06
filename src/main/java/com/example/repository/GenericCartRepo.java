a0d7ce28c3ae1aa5717a2ee994c28e495c62c179
Ashish Chodankar <acchodankar@gmail.com>
2022-04-06 17:25:12 +0530

    cart api added

--- src/main/java/com/example/repository/GenericCartRepo.java
+++ src/main/java/com/example/repository/GenericCartRepo.java
@@ -3,7 +3,9 @@ package com.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.entity.Cart;
+import com.example.entity.User;
 
 public interface GenericCartRepo extends JpaRepository<Cart,Integer> {
 
+	public Cart findByUser(User u);
 }
