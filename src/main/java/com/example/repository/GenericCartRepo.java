219dbcfc14fe562358379aafad91832f4a17c85f
try.it.out.cdac@gmail.com <try.it.out.cdac@gmail.com>
2022-04-06 17:50:25 +0530

    gen cart repo

--- src/main/java/com/example/repository/GenericCartRepo.java
+++ src/main/java/com/example/repository/GenericCartRepo.java
@@ -1,9 +1,11 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.entity.Cart;
 
+@Repository
 public interface GenericCartRepo extends JpaRepository<Cart,Integer> {
 
 }
